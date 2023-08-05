36ba1cd739b129ff4d238829ec1ecbfaf899afe0
Sergey Artemov <sergey560113@gmail.com>
2023-08-05 16:29:06 +0300

    Задание завершил

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,29 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+
+        userService.saveUser("Сергей", "Артемов", (byte) 29);
+        userService.saveUser("Анастасия", "Щучкина", (byte) 26);
+        userService.saveUser("Максим", "Лапшин", (byte) 26);
+        userService.saveUser("Азат", "Байгельдин", (byte) 28);
+
+        System.out.println(userService.getAllUsers());
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
-}
+    }
+
